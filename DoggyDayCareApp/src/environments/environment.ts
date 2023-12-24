619cb7b3c9db501809f70209ee58fee2f7425ca2
NatWhitt <116514364+NatWhitt@users.noreply.github.com>
2023-12-24 04:44:25 +0000

    Trying https call

--- DoggyDayCareApp/src/environments/environment.ts
+++ DoggyDayCareApp/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment ={
     production: false,
     // apiUrl: "https://localhost:7232/api",
     // apiUrlwithoutapi: "https://localhost:7232"
-    apiUrl: "http://nwhitt-001-site2.itempurl.com/api",
-    apiUrlwithoutapi: "http://nwhitt-001-site2.itempurl.com"
+    apiUrl: "https://nwhitt-001-site2.itempurl.com/api",
+    apiUrlwithoutapi: "https://nwhitt-001-site2.itempurl.com"
 };
