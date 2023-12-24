9fe41e4ec81028a9855aa3e6f7f6231a7b9247db
NatWhitt <116514364+NatWhitt@users.noreply.github.com>
2023-12-24 04:13:34 +0000

    UpdateURL

--- DoggyDayCareApp/src/environments/environment.ts
+++ DoggyDayCareApp/src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment ={
     production: false,
-    apiUrl: "https://localhost:7232/api",
-    apiUrlwithoutapi: "https://localhost:7232"
+    // apiUrl: "https://localhost:7232/api",
+    // apiUrlwithoutapi: "https://localhost:7232"
+    apiUrl: "http://nwhitt-001-site2.itempurl.com/api",
+    apiUrlwithoutapi: "http://nwhitt-001-site2.itempurl.com"
 };
